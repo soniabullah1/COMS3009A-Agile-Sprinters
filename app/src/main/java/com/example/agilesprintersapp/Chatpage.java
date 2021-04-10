22a58e746ea2979cbdd8ade7835155dd8348d22e
RushilPatel0703 <rushilpatel0703@gmail.com>
2021-04-10 04:11:15 +0200

    android studio is connected to lamp server and can recieve queried data from lamp

--- app/src/main/java/com/example/agilesprintersapp/Chatpage.java
+++ app/src/main/java/com/example/agilesprintersapp/Chatpage.java
@@ -67,64 +67,39 @@ public class Chatpage extends AppCompatActivity {
     }
 
     private void getJSON(final String urlWebService) {
-        /*
-         * As fetching the json string is a network operation
-         * And we cannot perform a network operation in main thread
-         * so we need an AsyncTask
-         * The constrains defined here are
-         * Void -> We are not passing anything
-         * Void -> Nothing at progress update as well
-         * String -> After completion it should return a string and it will be the json string
-         * */
+
         class GetJSON extends AsyncTask<Void, Void, String> {
 
-            //this method will be called before execution
-            //you can display a progress bar or something
-            //so that user can understand that he should wait
-            //as network operation may take some time
             @Override
             protected void onPreExecute() {
                 super.onPreExecute();
             }
 
-            //this method will be called after execution
-            //so here we are displaying a toast with the json string
             @Override
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
                 Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
             }
 
-            //in this method we are fetching the json string
             @Override
             protected String doInBackground(Void... voids) {
 
-
-
                 try {
-                    //creating a URL
                     URL url = new URL("https://lamp.ms.wits.ac.za/home/s2141916/test_WHATSAPP.php");
 
-                    //Opening the URL using HttpURLConnection
                     HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
-                    //StringBuilder object to read the string from the service
                     StringBuilder sb = new StringBuilder();
 
-                    //We will use a buffered reader to read the string from service
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
-                    //A simple string to read values from each line
                     String json;
 
-                    //reading until we don't find null
                     while ((json = bufferedReader.readLine()) != null) {
 
-                        //appending it to string builder
                         sb.append(json + "\n");
                     }
 
-                    //finally returning the read string
                     return sb.toString().trim();
                 } catch (Exception e) {
                     return null;
@@ -132,8 +107,6 @@ public class Chatpage extends AppCompatActivity {
 
             }
         }
-
-        //creating asynctask object and executing it
         GetJSON getJSON = new GetJSON();
         getJSON.execute();
     }
