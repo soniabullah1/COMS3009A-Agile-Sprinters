5ba5f0ad1f79c4247c10af0338d02a2f08ab61d8
RushilPatel0703 <rushilpatel0703@gmail.com>
2021-05-02 01:39:28 +0200

    more unit testing

--- app/src/androidTest/java/com/example/agilesprintersapp/MainActivityTest.java
+++ app/src/androidTest/java/com/example/agilesprintersapp/MainActivityTest.java
@@ -1,5 +1,7 @@
 package com.example.agilesprintersapp;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.view.View;
 
 import androidx.test.rule.ActivityTestRule;
@@ -9,6 +11,10 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.*;
 
 public class MainActivityTest {
@@ -17,6 +23,8 @@ public class MainActivityTest {
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
 
     private MainActivity mainActivity = null;
+
+    Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(RegisterActivity.class.getName(),null ,false);
 
     @Before
     public void setUp() throws Exception {
@@ -28,6 +36,15 @@ public class MainActivityTest {
     public void testLaunch(){
         View view = mainActivity.findViewById(R.id.tSignIn);
         assertNotNull(view);
+    }
+
+    @Test
+    public void testButtonClickRegister(){
+        onView(withId(R.id.tRegister)).perform(click());
+        Activity registerActivity = getInstrumentation().waitForMonitorWithTimeout(monitor,5000);
+        assertNotNull(registerActivity);
+
+        registerActivity.finish();
     }
 
 
