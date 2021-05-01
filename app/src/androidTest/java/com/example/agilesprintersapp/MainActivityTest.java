74911302214cbb3a899a99e6b63e2bab46ea75c8
RushilPatel0703 <rushilpatel0703@gmail.com>
2021-05-01 21:43:25 +0200

    checking if copveralls picks up test cases

---
+++ app/src/androidTest/java/com/example/agilesprintersapp/MainActivityTest.java
@@ -0,0 +1,38 @@
+package com.example.agilesprintersapp;
+
+import android.view.View;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    private MainActivity mainActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mainActivity = mainActivityActivityTestRule.getActivity();
+
+    }
+
+    @Test
+    public void testLaunch(){
+        View view = mainActivity.findViewById(R.id.tSignIn);
+        assertNotNull(view);
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+        mainActivity = null;
+    }
+}
