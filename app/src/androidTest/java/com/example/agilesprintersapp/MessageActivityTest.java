342c8aee5eadc73dd17d181beacb3687985b1499
RushilPatel0703 <rushilpatel0703@gmail.com>
2021-05-20 14:33:33 +0200

    More Message Activity testing

--- app/src/androidTest/java/com/example/agilesprintersapp/MessageActivityTest.java
+++ app/src/androidTest/java/com/example/agilesprintersapp/MessageActivityTest.java
@@ -2,6 +2,7 @@ package com.example.agilesprintersapp;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.test.rule.ActivityTestRule;
 
@@ -10,6 +11,12 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 public class MessageActivityTest {
@@ -20,10 +27,11 @@ public class MessageActivityTest {
 
 
 
+
     @Before
     public void setUp() throws Exception {
         messageActivity = messageActivityTestRule.getActivity();
-        messageActivity.sendBroadcast(new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS));
+        //messageActivity.sendBroadcast(new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS));
 
     }
 
@@ -33,12 +41,27 @@ public class MessageActivityTest {
         assertNotNull(view);
     }
 
-
     @Test
     public void testLaunchEditText_Send(){
         View view = messageActivity.findViewById(R.id.text_send);
         assertNotNull(view);
 
+    }
+
+//    @Test
+////    public void testCheckDialogDisplayed() {
+////        // Click on the button that shows the dialog
+////        onView(withId(R.id.btn_attach_pic)).perform(click());
+////
+////        // Check the dialog title text is displayed
+////        onView(withText(R.string.)).check(matches(isDisplayed()));
+////    }
+
+    @Test
+    public void testMessageSendButton(){
+        onView(withId(R.id.btn_send)).perform(click());
+        boolean toastMade = messageActivity.toastMade;
+        assertEquals(true, toastMade);
     }
 
     @After
