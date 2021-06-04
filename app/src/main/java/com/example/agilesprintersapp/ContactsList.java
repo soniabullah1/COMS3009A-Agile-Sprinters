88e2b6d697829e6debbc33e0fa973cdfc5eb1fbb
RushilPatel0703 <rushilpatel0703@gmail.com>
2021-06-05 01:09:47 +0200

    Unit testing

--- app/src/main/java/com/example/agilesprintersapp/ContactsList.java
+++ app/src/main/java/com/example/agilesprintersapp/ContactsList.java
@@ -1,32 +1,17 @@
 package com.example.agilesprintersapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListView;
 
 import androidx.annotation.NonNull;
-
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.agilesprintersapp.Adapter.UserAdapter;
 import com.example.agilesprintersapp.Model.User;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -35,8 +20,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 
 import java.util.ArrayList;
 import java.util.List;
