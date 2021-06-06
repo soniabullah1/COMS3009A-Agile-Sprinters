631883fcca77e1b112d7a15f31782715f0e04823
RushilPatel0703 <rushilpatel0703@gmail.com>
2021-06-07 01:42:35 +0200

    Unit Testing

--- app/src/main/java/com/example/agilesprintersapp/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/agilesprintersapp/Fragments/ProfileFragment.java
@@ -17,19 +17,13 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import com.bumptech.glide.Glide;
 import com.example.agilesprintersapp.ChangePasswordActivity;
-import com.example.agilesprintersapp.Model.User;
 import com.example.agilesprintersapp.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
@@ -76,34 +70,35 @@ public class ProfileFragment extends Fragment {
         storageReference = FirebaseStorage.getInstance().getReference("uploads");
 
 
-        fuser = FirebaseAuth.getInstance().getCurrentUser();
-        if (fuser != null) {
-            reference = FirebaseDatabase.getInstance().getReference("User").child(fuser.getUid());
-
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                User user = snapshot.getValue(User.class);
-                username.setText(user.getUsername());
-                contactNumber.setText(user.getContactNumber());
-                email.setText(user.getEmail());
-
-                if (user != null && user.getId() != null) {
-                    if (user.getImageURL().equals("default")) {
-                        image_profile.setImageResource(R.mipmap.ic_launcher);
-                    } else {
-                        if(getActivity() == null){
-                            return;
-                        }
-                        Glide.with(getContext()).load(user.getImageURL()).into(image_profile);
-                    }
-                }
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });}
+//        fuser = FirebaseAuth.getInstance().getCurrentUser();
+//        if (fuser != null) {
+//            reference = FirebaseDatabase.getInstance().getReference("User").child(fuser.getUid());
+//
+//        reference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                User user = snapshot.getValue(User.class);
+//                username.setText(user.getUsername());
+//                contactNumber.setText(user.getContactNumber());
+//                email.setText(user.getEmail());
+//
+//                if (user != null && user.getId() != null) {
+//                    if (user.getImageURL().equals("default")) {
+//                        image_profile.setImageResource(R.mipmap.ic_launcher);
+//                    } else {
+//                        if(getActivity() == null){
+//                            return;
+//                        }
+//                        Glide.with(getContext()).load(user.getImageURL()).into(image_profile);
+//                    }
+//                }
+//            }
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//        }
 
         edit_profile_image.setOnClickListener(new View.OnClickListener() {
             @Override
