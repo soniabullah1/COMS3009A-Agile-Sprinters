bd5f66796e82a2ead150d1da6410aaf5109e3c96
RushilPatel0703 <rushilpatel0703@gmail.com>
2021-06-16 03:15:34 +0200

    Testing Profile

--- app/src/androidTest/java/com/example/agilesprintersapp/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/agilesprintersapp/LoginActivityTest.java
@@ -181,9 +181,9 @@ public class LoginActivityTest {
             e.printStackTrace();
         }
 
-        onView(withId(R.id.edit_email)).perform(typeText(STRING_TO_BE_TYPED_EMAIL1), closeSoftKeyboard());
-        onView(withId(R.id.edit_phone_number)).perform(typeText(STRING_TO_BE_TYPED_PHONE), closeSoftKeyboard());
-        onView(withId(R.id.edit_username)).perform(typeText(STRING_TO_BE_TYPED_USERNAME), closeSoftKeyboard());
+//        onView(withId(R.id.edit_email)).perform(typeText(STRING_TO_BE_TYPED_EMAIL1), closeSoftKeyboard());
+//        onView(withId(R.id.edit_phone_number)).perform(typeText(STRING_TO_BE_TYPED_PHONE), closeSoftKeyboard());
+//        onView(withId(R.id.edit_username)).perform(typeText(STRING_TO_BE_TYPED_USERNAME), closeSoftKeyboard());
         onView(withId(R.id.btn_saver)).perform(click(), closeSoftKeyboard());
 
     }
